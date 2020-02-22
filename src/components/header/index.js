d9004113318314f825d21b292aabc61d2393ac0c
Prashanth <venkateshprasads626@gmail.com>
2020-02-22 15:59:42 +0530

    first commit

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,9 +2,12 @@
 import React, { Component } from 'react'
 import logo from '../../logo.svg';
 import { NavLink } from "react-router-dom"
+import $ from 'jquery'
 
 
 class Header extends Component{
+
+
     render() {
         return(
             <header className="menu">
@@ -17,23 +20,18 @@ class Header extends Component{
                 </div>
                      
                 <ul className="nav-links" id="mynav">
-                <li className="active">
-                    <NavLink to="/">Home<span className="dot dot1"><i class="fa fa-home" aria-hidden="true"></i></span></NavLink>
-                   
-                </li>
-                <li>
-                    <NavLink to="/products">Our Products<span className="dot dot2"><i className="fa fa-desktop" aria-hidden="true"></i></span></NavLink>
-                   
-                    
-                </li>
-                <li>
-                    <NavLink to="/Aboutus">About Us<i class="fa fa-user-circle" aria-hidden="true"></i></NavLink>
-                    
-                </li>
-                <li>
-                    <NavLink to="/Contact">Contact Us<span className="dot dot3"><i class="fa fa-phone" aria-hidden="true"></i></span></NavLink>
-                    
-                </li>
+                    <li className="active">
+                        <NavLink exact to="/" activeClassName="main-nav-active">Home<span className="dot dot1"><i class="fa fa-home" aria-hidden="true"></i></span></NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/products" activeClassName="main-nav-active">Our Products<span className="dot dot2"><i className="fa fa-desktop" aria-hidden="true"></i></span></NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/Aboutus" activeClassName="main-nav-active">About Us<i class="fa fa-user-circle" aria-hidden="true"></i></NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/Contact" activeClassName="main-nav-active">Contact Us<span className="dot dot3"><i class="fa fa-phone" aria-hidden="true"></i></span></NavLink>
+                    </li>
                 </ul>
             </nav>          
             </div>
